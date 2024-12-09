0ba79ba222fa78f4104c042c0d4ed50af080ba6b
Micro-Organism <coolrabbit1993@gmail.com>
2024-12-09 19:17:38 +0800

    feat: springboot 集成 gzip

---
+++ zero-zip-gzip/src/main/java/com/zero/zip/gzip/ZeroZipGzipApplication.java
@@ -0,0 +1,13 @@
+package com.zero.zip.gzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroZipGzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroZipGzipApplication.class, args);
+    }
+
+}
---
+++ zero-zip-gzip/src/test/java/com/zero/zip/gzip/ZeroZipGzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.zip.gzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroZipGzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
